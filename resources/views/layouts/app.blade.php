2572df19fb0bbe594558994563be4ce669ae4088
Mario Moreno <mario_morenor@hotmail.com>
2020-09-29 14:06:51 -0500

    vista layout modificada

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -63,6 +63,7 @@
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
+                                    
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                         @csrf
