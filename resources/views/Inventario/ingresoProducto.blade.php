d7d8e7a51668a40af9ee9a598f3378be384b46fb
Ceider <cgzambranoa@pucesd.edu.ec>
2020-10-02 14:42:29 -0500

    sss

--- resources/views/Inventario/ingresoProducto.blade.php
+++ resources/views/Inventario/ingresoProducto.blade.php
@@ -4,7 +4,8 @@
 @endpush
 @section('panel')
 <div class="container">
-    <form action="{{ route('products.store') }}" method="POST" class="mt-3 rounded border py-3 px-5 shadow-lg col bg-white">
+    <form action="{{ route('products.store') }}" method="POST"
+        class="mt-3 rounded border py-3 px-5 shadow-lg col bg-white">
         @csrf
         <div>
             <h2 class="h2_producto">INGRESO DE PRODUCTOS</h2>
@@ -15,13 +16,14 @@
                 <input type="text" value="{{ old('code') }}" class="form-control @error('code') is-invalid @enderror"
                     name="code" maxlength="15" placeholder="COD001">
                 @error('code')
-                <div class="alert alert-danger">{{ $message }}</div>
+                <span class="text-danger">{{ $message }}</span>
                 @enderror
             </div>
             <div class="form-group col-md-3">
-              <label for="inputAddress2">Cantidad: </label>
-              <input type="number" value="{{ old('total_amount') }}" name="total_amount" class="form-control" step="1" placeholder="1" min="1">
-              @error('total_amount')
+                <label for="inputAddress2">Cantidad: </label>
+                <input type="number" value="{{ old('total_amount') }}" name="total_amount" class="form-control" step="1"
+                    placeholder="1" min="1">
+                @error('total_amount')
                 <div class="alert alert-danger">{{ $message }}</div>
                 @enderror
             </div>
@@ -57,7 +59,7 @@
                 <label>Precio normal: </label>
                 <input type="number" value="{{ old('regular_price') }}" name="regular_price" class="form-control"
                     step="0.01" placeholder="10.00" min="0.01" max="99999.99">
-                    @error('regular_price')
+                @error('regular_price')
                 <div class="alert alert-danger">{{ $message }}</div>
                 @enderror
             </div>
@@ -65,7 +67,7 @@
                 <label>Precio venta: </label>
                 <input type="number" value="{{ old('sale_price') }}" name="sale_price" class="form-control" step="0.01"
                     placeholder="10.00" min="0.01" max="99999.99">
-                    @error('sale_price')
+                @error('sale_price')
                 <div class="alert alert-danger">{{ $message }}</div>
                 @enderror
             </div>
@@ -95,13 +97,11 @@
 
 @push('js')
 <script>
-// SideBar
-        mostrar_submenu($('#sidebar-container ul .acciones-show')); //Muestra El submenu ACCIONES
-        menu_activo($('.acciones-btn')) //Sombrea el menu ACCIONES
-        submenu_activo($('#link_crear_articulo')); //Se pone en negrita la opcion Listado Articulo
-// **********
-
-
+    // SideBar
+    mostrar_submenu($('#sidebar-container ul .acciones-show')); //Muestra El submenu ACCIONES
+    menu_activo($('.acciones-btn')) //Sombrea el menu ACCIONES
+    submenu_activo($('#link_crear_articulo')); //Se pone en negrita la opcion Listado Articulo
+    // **********
 </script>
 
 @endpush
