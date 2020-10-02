3a217b1eccdde0bb8680950e16f66fdf74eec551
Mario Moreno <mario_morenor@hotmail.com>
2020-10-02 15:03:18 -0500

    Validacion precios

--- app/Http/Requests/ProductRequest.php
+++ app/Http/Requests/ProductRequest.php
@@ -24,7 +24,9 @@ class ProductRequest extends FormRequest
             "name"=>"required|min:4|max:15",
             "description"=>"max:250",
             "short_description"=>"max:50",
-            "total_amount"=>"required|numeric|min:1|max:5000"
+            "total_amount"=>"required|numeric|min:1|max:5000",
+            "regular_price"=>"required|numeric|min:0.01|max:9999",
+            "sale_price"=>"required|numeric|min:0.01|max:9999"
         ];
     }
 
@@ -42,6 +44,14 @@ class ProductRequest extends FormRequest
             'total_amount.numeric'=>'El campo :attribute debe ser un número.',
             'total_amount.min'=>'El campo :attribute debe ser mayor a 0.',
             'total_amount.max'=>'El campo :attribute no debe exceder los 5000.',
+            'regular_price.required'=>'El campo :attribute no debe estar vacío.',
+            'regular_price.numeric'=>'El campo :attribute debe ser numérico.',
+            'regular_price.min'=>'El campo :attribute debe ser mayor a 0.01.',
+            'regular_price.max'=>'El campo :attribute debe ser menor a 9999.',
+            'sale_price.required'=>'El campo :attribute no debe estar vacío.',
+            'sale_price.numeric'=>'El campo :attribute debe ser numérico.',
+            'sale_price.min'=>'El campo :attribute debe ser mayor a 0.01.',
+            'sale_price.max'=>'El campo :attribute debe ser menor a 9999.',
 
         ];
     }
@@ -53,7 +63,9 @@ class ProductRequest extends FormRequest
             'name'=> 'Nombre del Producto',
             'description'=> 'Descripción del Producto',
             'short_description'=>'Descripción Corta',
-            'total_amount'=>'Cantidad de Stock'
+            'total_amount'=>'Cantidad de Stock',
+            'regular_price'=>'Precio Regular',
+            'sale_price'=>'Precio Venta',
         ];
     }
 }
